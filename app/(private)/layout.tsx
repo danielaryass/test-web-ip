a6311ab4151cfce379b750c0e03bfffcb05434d4
danielaryass <daniel.aryass7@gmail.com>
2025-06-17 20:35:33 +0700

    First commit

---
+++ app/(private)/layout.tsx
@@ -0,0 +1,22 @@
+import { IP_CAN_ACCESS } from "@/const/const";
+import { headers } from "next/headers";
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const headersList = headers();
+  const IP = headersList.get("x-forwarded-for") as string;
+  const canAccess = IP_CAN_ACCESS.includes(IP);
+
+  if (!canAccess) {
+     return <>
+     <p>Access Denied your ip is {IP} </p>
+     <ol>
+        {IP_CAN_ACCESS.map((ip) => <li key={ip}>{ip}</li>)}
+     </ol>
+     </>
+  }
+  return <>{children}</>;
+}
